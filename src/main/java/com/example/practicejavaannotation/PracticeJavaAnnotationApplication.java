c85274526e5046e516417f259c192ff36303c680
kimsikkong <kimsikkong@gmail.com>
2021-08-13 22:58:49 +0900

    initial commit

---
+++ src/main/java/com/example/practicejavaannotation/PracticeJavaAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicejavaannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJavaAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJavaAnnotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicejavaannotation/PracticeJavaAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicejavaannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJavaAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
